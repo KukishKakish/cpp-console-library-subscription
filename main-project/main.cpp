7d3a99fdfa26d4a019070202b38c7dd9add18423
kukish <notgon36@gmail.com>
2023-12-25 16:36:53 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #5. Library Subscription\n";
     cout << "Author: Krapiventsew D\n";
+    cout << "Group: PInj23_1\n";
     return 0;
 }
